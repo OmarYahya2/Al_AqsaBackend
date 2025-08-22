a44e2aa5ba5d6a47de9949e1ca3b6e04831a9524
OmarYahya2 <omar.yahya965@gmail.com>
2025-08-22 16:03:52 +0300

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ContactMessageView
+from .views import ContactMessageView, APIInfoView
 
 urlpatterns = [
     path('contact/', ContactMessageView.as_view(), name='contact'),
+    path('info/', APIInfoView.as_view(), name='api_info'),
 ]
